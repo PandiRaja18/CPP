31088f4056d3b1ec3232a88906966c65b376bb91
New World <68177808+PandiRaja18@users.noreply.github.com>
2023-05-16 18:38:01 +0530

    Add files via upload

---
+++ getInputUntilnewline.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+int main()
+{
+	char str[30];
+	int t = 4;
+	cin.getline(str,10);
+		cout << str << " : newline" << endl;
+	return 0;
+}
